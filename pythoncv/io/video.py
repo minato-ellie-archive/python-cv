5486477afda55cef023d4756528bd8b02a7f4d2e
Minato <82735346+minato-ellie@users.noreply.github.com>
2023-03-27 13:11:41 +0900

    refactor: refactor Video class methods and add type annotations
    - Refactor the `generate_info_wrapper` function to `_generate_info_wrapper`
    - Add type annotations to the `info` method in the `Video` class
    - Change the type of the `__len__` method in the `Video` class to Optional[int]

--- pythoncv/io/video.py
+++ pythoncv/io/video.py
@@ -1,14 +1,29 @@
 import os
 from abc import ABCMeta, abstractmethod
-from typing import Union, Literal
+from typing import Union, Optional, Tuple
 
-import numpy as np
 import cv2
+import numpy as np
 
-from pythoncv.models import VideoCaptureProperties, CaptureBackends, CAPTURE_BACKEND_DICT
+from pythoncv.types import (
+    VideoCaptureProperties,
+    CaptureBackends,
+    CAPTURE_BACKEND_DICT,
+    FourCC,
+)
 
 
-def generate_info_wrapper(cap: cv2.VideoCapture):
+def _generate_info_wrapper(cap: cv2.VideoCapture):
+    """
+    Generates a wrapper, which make a fake VideoCaptureProperties object, which can be used to get and set
+    properties of the VideoCapture object.
+
+    Args:
+        cap: VideoCapture object
+
+    Returns:
+        A wrapper object, which can be used to get and set properties of the VideoCapture object.
+    """
     properties = VideoCaptureProperties()
 
     def __getattribute__(self, item):
@@ -30,7 +45,7 @@ def generate_info_wrapper(cap: cv2.VideoCapture):
                    f"fps: {self.fps}, width: {self.frame_width}, height: {self.frame_height}, "
                    f"frame_count: {self.frame_count})")
 
-    return type('VideoCaptureProperties', (object, ), {
+    return type('VideoCaptureProperties', (object,), {
         '__doc__': VideoCaptureProperties.__doc__,
         '__getattribute__': __getattribute__,
         '__setattr__': __setattr__,
@@ -39,13 +54,37 @@ def generate_info_wrapper(cap: cv2.VideoCapture):
 
 
 class BaseVideo(metaclass=ABCMeta):
-    @abstractmethod
-    def __iter__(self):
-        ...
+    """
+    Base class for video.
+
+    A video in pythoncv is a generator, which yields a frame(a numpy.ndarray object) each time.
+
+    Notes:
+        Image in pythoncv is a numpy.ndarray object, which has the shape of (height, width, channel).
+        The channel of the image is RGB, which is different from the channel of the image in OpenCV,
+        but the same as the channel of the image in PIL and Tensorflow.
+
+    Attributes:
+        path: Path to the video file.
+        fps: Frames per second.
+        wait_time: Time to wait between each frame.
+        info: VideoCaptureProperties object, which can be used to get and set properties of the VideoCapture object.
+
+    Methods:
+        __next__: Get the next frame.
+        __iter__: Get the iterator of the video.
+        __len__: Get the length of the video.
+    """
 
     @abstractmethod
-    def __len__(self):
+    def __next__(self):
         ...
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return NotImplemented
 
     @property
     def fps(self) -> float:
@@ -77,6 +116,39 @@ class BaseVideo(metaclass=ABCMeta):
 
 
 class Video(BaseVideo):
+    """
+    Pythonic API for video.
+
+    Notes:
+        Image in pythoncv is a numpy.ndarray object, which has the shape of (height, width, channel).
+        The channel of the image is RGB, which is different from the channel of the image in OpenCV,
+        but the same as the channel of the image in PIL and Tensorflow.
+
+        OpenCV Capture will be released automatically when the Video object is deleted, by using the GC mechanism.
+
+    Args:
+        path: Path to the video file.
+        backend: Backend to use for capturing video.
+            If backend is "auto", the backend will be chosen automatically.
+            If backend is "opencv", the backend will be OpenCV.
+            If backend is "ffmpeg", the backend will be ffmpeg.
+            If backend is "gstreamer", the backend will be gstreamer.
+            other backend types can be found in `pythoncv.types.CaptureBackends``
+
+    Attributes:
+        path: Path to the video. If the video is read from a device, the path will be the device number.
+        fps: Frames per second. When you set the fps, the wait_time will be changed automatically.
+        wait_time: Time to wait between each frame.
+        info: VideoCaptureProperties object, which can be used to get and set properties of the VideoCapture object.
+
+    Methods:
+        __next__: Get the next frame.
+        __iter__: Get the iterator of the video.
+        __len__:
+            Get the length of the video.
+            If the video is read from a device(which has unlimited length), the length will be set to None.
+    """
+
     def __init__(
         self,
         path: Union[str, int],
@@ -87,7 +159,7 @@ class Video(BaseVideo):
 
         fps = self._cap.get(cv2.CAP_PROP_FPS)
         self._wait_time = 1 / fps if fps > 0 else 0
-        self._info = generate_info_wrapper(self._cap)
+        self._info = _generate_info_wrapper(self._cap)
 
     @property
     def wait_time(self) -> float:
@@ -105,15 +177,14 @@ class Video(BaseVideo):
     def info(self, value):
         self._info = value
 
-    def __iter__(self):
-        while self._cap.isOpened():
-            ret, frame = self._cap.read()
-            if ret:
-                yield frame
-            else:
-                break
+    def __next__(self) -> np.ndarray:
+        ret, frame = self._cap.read()
+        if ret:
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        else:
+            raise StopIteration
 
-    def __len__(self):
+    def __len__(self) -> Optional[int]:
         return int(self._cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def __del__(self):
@@ -124,6 +195,28 @@ def read_video_from_device(
     device: int,
     backend: CaptureBackends = "auto",
 ) -> Video:
+    """
+    Read video from a device.
+
+    Args:
+        device: Device number. Most times, your camera is 0.
+        backend: Backend to use for capturing video.
+
+    Returns:
+        A Video object.
+
+    Raises:
+        TypeError: If device is not an int or str.
+        ValueError: If device is not a positive integer.
+
+    Examples:
+        >>> video = read_video_from_device(0)
+        >>> for frame in video:
+        >>>     print(frame.shape)
+        (480, 640, 3)
+        (480, 640, 3)
+        ...
+    """
     if isinstance(device, str):
         device = int(device)
     elif not isinstance(device, int):
@@ -136,9 +229,31 @@ def read_video_from_device(
 
 
 def read_video_from_file(
-    path: os.PathLike,
+    path: Union[str, os.PathLike],
     backend: CaptureBackends = "auto",
 ) -> Video:
+    """
+    Read video from a file.
+
+    Args:
+        path: Path to the video file.
+        backend: Backend to use for capturing video.
+
+    Returns:
+        A Video object.
+
+    Raises:
+        TypeError: If path is not a string.
+        FileNotFoundError: If the file is not found.
+
+    Examples:
+        >>> video = read_video_from_file("video.mp4")
+        >>> for frame in video:
+        >>>     print(frame.shape)
+        (1080, 1920, 3)
+        (1080, 1920, 3)
+        ...
+    """
     if not isinstance(path, str):
         try:
             path = str(path)
@@ -155,7 +270,29 @@ def read_video_from_url(
     url: str,
     backend: CaptureBackends = "auto",
 ) -> Video:
+    """
+    Read video from a url.
+
+    Args:
+        url: Url to the video.
+        backend: Backend to use for capturing video.
+
+    Returns:
+        A Video object.
+
+    Raises:
+        TypeError: If url is not a string.
+
+    Examples:
+        >>> video = read_video_from_url("https://mazwai.com/videvo_files/video/free/2018-12/small_watermarked/180607_A_124_preview.mp4") # noqa E501
+        >>> for frame in video:
+        >>>     print(frame.shape)
+        (1080, 1920, 3)
+        (1080, 1920, 3)
+        ...
+    """
     if not isinstance(url, str):
         raise TypeError(f"url must be a string, not {type(url)}")
 
     return Video(url, backend=backend)
+
