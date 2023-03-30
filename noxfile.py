ea31f40d98908c2ec2e40ce5a81ba5d3e25f8017
Minato <82735346+minato-ellie@users.noreply.github.com>
2023-03-30 18:35:08 +0900

    ci: add Codecov coverage reporting to Python tests
    - Add a Github Action for Python tests
    - Rename tests session to unit_tests in noxfile.py
    - Generate and upload a coverage report for Codecov in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2,18 +2,24 @@ import nox
 
 
 @nox.session
-def tests(session):
+def unit_tests(session):
     session.install('poetry')
     session.run('poetry', 'install')
-    session.run('poetry', 'run', 'pytest', '--cov=pythoncv', '--cov-report=term-missing', '--cov-report=html')
+    # Generate coverage report for Codecov
+    session.run('poetry', 'run', 'pytest',
+                '--cov=pythoncv',
+                '--cov-report=term-missing',
+                '--cov-report=xml',
+                '--cov-fail-under=75'
+                )
     session.notify('coverage')
 
 
 @nox.session
 def coverage(session):
-    session.install('coverage')
-    session.run('coverage', 'report', '--show-missing', '--fail-under=75')
-    session.run('coverage', 'erase')
+    session.install('coverage', 'codecov')
+    session.run('coverage', 'xml', '--fail-under=75')
+    session.run('codecov')
 
 
 @nox.session
