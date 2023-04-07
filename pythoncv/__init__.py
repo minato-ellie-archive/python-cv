714f5467eaa657dc693a00b3874eda442757ccf8
Minato <82735346+minato-ellie@users.noreply.github.com>
2023-04-07 19:16:11 +0900

    refactor: import CVImage and CVVideo from pythoncv.core
    - Import CVImage and CVVideo from pythoncv.core

--- pythoncv/__init__.py
+++ pythoncv/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for Pythonic Vision."""
 
+from pythoncv.core import CVImage, CVVideo
+
 __author__ = """Minato"""
 __email__ = '82735346+minato-ellie@users.noreply.github.com'
 __version__ = '0.1.0'
